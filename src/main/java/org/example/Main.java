f9726eaf7f27548e3cbb0ade49182b792bf06a8c
leandro.reis <lr862644@gmail.com>
2024-12-11 11:57:17 -0300

    Soma de dois numeros

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    float atividade1;
+
+    atividade1 = atividade.atividade1();
+    System.out.println(atividade1);
+
+    }
+}
---
+++ src/main/java/org/example/atividade.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class atividade {
+    public static Float atividade1() {
+        Scanner scanner = new Scanner(System.in);
+        /*
+        Exercício 1: Soma de Dois Números
+        Peça ao usuário para digitar dois números inteiros e mostre a soma dos
+        dois números.
+        Utilize Scanner para entrada e System.out.println para saída.
+        */
+
+        System.out.println("Digite o primeiro numero a ser somado: ");
+        float n1 = scanner.nextFloat();
+
+        System.out.println("Digite o segundo numero a ser somado: ");
+        float n2 = scanner.nextFloat();
+
+        return n1 +n2;
+    }
+}
+
+
+
